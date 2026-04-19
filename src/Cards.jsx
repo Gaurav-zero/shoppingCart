3a62d9f81ef4556e5a8d18cba4ec17ee541ce818
Gaurav <183895839+Gaurav-zero@users.noreply.github.com>
2026-04-19 14:20:11 +0530

    add Product details
    used fakeStoreAPI to get product details and added
    them to ShopPage

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,12 +1,30 @@
-const Cards= ({title, price, image, desc, handleaddToCart}) => {
+import { useEffect, useState } from "react";
+
+const Cards= ({ind, handleAddToCart}) => {
+    const [title, setTitle]= useState("");
+    const [desc, setDesc]= useState("");
+    const [price, setPrice]= useState("");
+    const [imgUrl, setImgUrl]= useState("");
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(response => response.json())
+            .then(data => {
+                setDesc(data[ind].description);
+                setTitle(data[ind].title);
+                setPrice(data[ind].price);
+                setImgUrl(data[ind].image);
+            });
+    }, []);
+    
     return (
         <div>
-            <img src={image} alt="no image yet" />
+            <img src={imgUrl} alt="no image yet" />
             <h1>{title}</h1>
             <p>{desc}</p>
-            <h2>Rs.{price}</h2>
+            <h2>${price}</h2>
 
-            <button onClick={handleaddToCart}>Add to Cart</button>
+            <button onClick={handleAddToCart}>Add to Cart</button>
         </div>
     );
 };
--- src/ShopPage.jsx
+++ src/ShopPage.jsx
@@ -1,13 +1,18 @@
 import NavBar from "./NavBar";
 import Cards from "./Cards";
+import { useEffect, useState } from "react";
 
-const ShopPage = () => {
+const ShopPage = () => {    
+    const [cards, setCards]= useState([0,1,2,3,4,5]);
+
     return (
         <div>
             <h1>Get best Merchandises</h1>
             <NavBar />
 
-            <Cards title="Polo Shirt" desc="none" price="1000" image="" />
+            {cards.map((ind) => (
+                <Cards ind={ind} />
+            ))}           
 
 
         </div>
