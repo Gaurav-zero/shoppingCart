142a7729c776f7804a3a434b4e1f5617aad6ec57
Gaurav <183895839+Gaurav-zero@users.noreply.github.com>
2026-04-19 17:25:51 +0530

    add CartArray
    defined an array state in Cart.jsx for storing all
    the elements in the Cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import NavBar from "./NavBar";
 
 const Cart= () => {
+    const [cartItems, setCartItems]= useState(["shirts"]);
+
     return (
         <div>
             <h1>Shopping Cart</h1>
             
             <NavBar />
+
+            <ul>
+                {cartItems.map((item, ind)=> {
+                    return <li>{ind+1}.{item}</li>
+                })}
+            </ul>
+            
         </div>
     );
 };
